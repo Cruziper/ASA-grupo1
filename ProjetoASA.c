104858999f0e3118781d7f7dc806e41bad523666
QuickMax <dragonmasaster@gmail.com>
2018-03-19 15:10:18 +0000

    Ole

--- ProjetoASA.c
+++ ProjetoASA.c
@@ -1,17 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct {
+  int v_index;
+  int in_stack;
+
+
+} vertice;
+
+typedef struct {
+  int vertice1;
+  struct vertice *next;
+} aresta;
+
+
+void stackPush (int v_index){
+
+
+}
+
+int stackPop(){
+
+}
+
+
 int main() {
   int NVertices = 0;
   int NArestas = 0;
 
-  scanf("%d\n", NVertices);
-  scanf("%d\n", NArestas);
+  scanf("%d\n", &NVertices);
+  scanf("%d\n", &NArestas);
 
-  
+  for (int i = 1; i <= NArestas; i++) {
+    scanf("%d %d\n", &vertice1, &vertice2);
+    addAresta(vertice1, vertice2);
+  }
 
   if(NVertices<2 && NArestas<1){
     return -1;
   }
+
+  printf("%d\n", NVertices );
+  printf("%d\n", NArestas);
+  //for (int i = 1; i <= ??; i++) {
+  //  printf("%s\n", ??);
+  //}
+
   return 0;
 }
