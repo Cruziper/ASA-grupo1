319e327ba5901f43e9d14b900157b3c0a9ca613f
Cruziper <goncalo.garcia.cruz@tecnico.ulisboa.pt>
2018-03-21 00:06:01 +0000

    Pequenas alteracoes de abstracao

--- projetoASA.c
+++ projetoASA.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 typedef struct {
   int v_index;
+  int d_u;
+  int low_u;
   int in_stack;
-
-
 } vertice;
 
 typedef struct {
@@ -23,19 +24,28 @@ int stackPop(){
 
 }
 
+void readData() {
+    vertice stack_array = [];
+    scanf("%d\n", &NVertices);
+    scanf("%d\n", &NArestas);
+
+    for (int i = 1; i <= NArestas; i++) {
+      scanf("%d %d\n", &vertice1, &vertice2);
+      addAresta(vertice1, vertice2);
+    }
+}
+
+void testArguments(){
+
+}
 
 int main() {
   int NVertices = 0;
   int NArestas = 0;
 
-  scanf("%d\n", &NVertices);
-  scanf("%d\n", &NArestas);
+  readData();
 
-  for (int i = 1; i <= NArestas; i++) {
-    scanf("%d %d\n", &vertice1, &vertice2);
-    addAresta(vertice1, vertice2);
-  }
-
+  testArguments();
   if(NVertices<2 && NArestas<1){
     return -1;
   }
