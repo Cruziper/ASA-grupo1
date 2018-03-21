07a139e49b74b8148db5ecaa8690cee990e1d578
Cruziper <goncalogarciacruz@hotmail.com>
2018-03-21 09:17:22 +0000

    Little fixes

--- projetoASA.c
+++ projetoASA.c
@@ -14,6 +14,26 @@ typedef struct {
   struct vertice *next;
 } aresta;
 
+void readData() {
+    scanf("%d\n", &N_vertices);
+    scanf("%d\n", &N_links);
+}
+
+void readData_Links(N_links){
+  for (int i = 1; i <= N_links; i++) {
+    scanf("%d %d\n", &vertice1, &vertice2);
+    addVertice(vertice1);
+    addVertice(vertice2);
+    addAresta(vertice1, vertice2);
+  }
+}
+
+void testArguments(N_vertices, N_links){
+  if(N_vertices < 2 || N_links < 1){
+    printf("Input incorreto\n", );
+    return 0;
+  }
+}
 
 void stackPush (int v_index){
 
@@ -24,34 +44,18 @@ int stackPop(){
 
 }
 
-void readData() {
-    vertice stack_array = [];
-    scanf("%d\n", &NVertices);
-    scanf("%d\n", &NArestas);
-
-    for (int i = 1; i <= NArestas; i++) {
-      scanf("%d %d\n", &vertice1, &vertice2);
-      addAresta(vertice1, vertice2);
-    }
-}
-
-void testArguments(){
-
-}
-
 int main() {
-  int NVertices = 0;
-  int NArestas = 0;
+  int N_vertices = 0;
+  int N_links = 0;
 
   readData();
 
-  testArguments();
-  if(NVertices<2 && NArestas<1){
-    return -1;
-  }
+  struct vertice stack_array [N_vertices];
 
-  printf("%d\n", NVertices );
-  printf("%d\n", NArestas);
+  testArguments();
+
+  printf("%d\n", N_vertices );
+  printf("%d\n", N_links);
   //for (int i = 1; i <= ??; i++) {
   //  printf("%s\n", ??);
   //}
