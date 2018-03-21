9f025ea2bdf83f985c1ca1ae24f236f2139be82a
QuickMax <dragonmasaster@gmail.com>
2018-03-21 15:29:50 +0000

    Grafo por testar

--- ProjetoASA.c
+++ ProjetoASA.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+
+#define novo_vertice (struct vertice*)malloc(sizeof(struct vertice))
+#define novo_grafo (struct Graph*) malloc(sizeof(struct Graph))
 
 typedef struct {
-  int v_index;
-  int in_stack;
-
-
+  int dest;
+  int v_origem;
+  int na_pilha;
+  int descoberta;
+  struct vertice *next;
 } vertice;
 
 typedef struct {
@@ -13,6 +18,42 @@ typedef struct {
   struct vertice *next;
 } aresta;
 
+typedef struct {
+  struct vertice *head;
+} listaAdja;
+
+typedef struct {
+  int Nvert;
+  struct listaAdja* arrayAdjList;
+} Grafo;
+
+struct vertice* criar_novo_vertice(int V){
+  struct vertice* novoVer = novo_vertice;
+  novoVer -> dest = dest;
+  novoVer -> next = NULL;
+  novoVer -> descoberta = INFINITY;
+  novoVer -> v_origem = NULL
+  return novoVer;
+}
+
+struct Grafo* criar_novo_grafo(int V) {
+  struct Grafo* novoGrafo = novo_grafo;
+  grafo -> V = V;
+
+  grafo -> arrayAdjList = (struct listaAdja*) malloc(V * sizeof(struct listaAdja));
+
+  int i;
+  for (i=0; i < V; i++)
+    grafo->arrayAdjList[i].head = NULL;
+
+    return grafo;
+  }
+
+void adiciona_aresta(struct Grafo* grafo, int origem, int destino){
+  struct vertice* novoVer = criar_novo_vertice(destino);
+  novoVer->next = grafo->arrayAdjList[origem].head;
+  grafo->arrayAdjList[destino].head = novoVer;
+}
 
 void stackPush (int v_index){
 
@@ -30,6 +71,11 @@ int main() {
 
   scanf("%d\n", &NVertices);
   scanf("%d\n", &NArestas);
+
+  struct vertice *listaAdja[NVertices], *p;
+
+  for( i = 1 ; i <= NVertices ; i++ )
+		listaAdja[i] = NULL;
 
   for (int i = 1; i <= NArestas; i++) {
     scanf("%d %d\n", &vertice1, &vertice2);
