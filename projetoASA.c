5fea2b0e793ef60f123aefbde156e9e807425059
Cruziper <goncalogarciacruz@hotmail.com>
2018-03-21 16:55:16 +0000

    Change Language, Fix Bugs, Restruct Code

--- projetoASA.c
+++ projetoASA.c
@@ -2,58 +2,63 @@
 #include <stdlib.h>
 #include <math.h>
 
-#define novo_vertice (struct vertice*)malloc(sizeof(struct vertice))
-#define novo_grafo (struct Graph*) malloc(sizeof(struct Graph))
+#define newVertexertex_malloc (struct Vertex*)malloc(sizeof(struct Vertex))
+#define newGraph_malloc (struct Graph*) malloc(sizeof(struct Graph))
+
+//> STRUCTS ////////////////////////////////////////////////////////////////////
+typedef struct {
+  int vIndex;
+  int d;
+  int low;
+  int in_stack; //0 (it isn't) or 1 (it is)
+  struct Vertex *next;
+} Vertex;
 
 typedef struct {
-  int dest;
-  int v_origem;
-  int na_pilha;
-  int descoberta;
-  struct vertice *next;
-} vertice;
+  int vertex;
+  struct Vertex *next;
+} Edge;
 
 typedef struct {
-  int vertice1;
-  struct vertice *next;
-} aresta;
-
-typedef struct {
-  struct vertice *head;
-} listaAdja;
+  struct Vertex *head;
+} ListaAdja;
 
 typedef struct {
   int Nvert;
-  struct listaAdja* arrayAdjList;
-} Grafo;
+  struct ListaAdja* arrayAdjList;
+} Graph;
+////////////////////////////////////////////////////////////////////////////////
 
-struct vertice* criar_novo_vertice(int V){
-  struct vertice* novoVer = novo_vertice;
-  novoVer -> dest = dest;
-  novoVer -> next = NULL;
-  novoVer -> descoberta = INFINITY;
-  novoVer -> v_origem = NULL
-  return novoVer;
+//> addVertex //////////////////////////////////////////////////////////////////
+struct Vertex* addVertex(int V){
+  struct Vertex* newVertex = newVertex_malloc;
+  newVertex -> vIndex = V;
+  newVertex -> d = NULL;
+  newVertex -> low = INFINITY;
+  newVertex -> in_stack = 0;
+  newVertex -> next = NULL;
+  return newVertex;
 }
 
-struct Grafo* criar_novo_grafo(int V) {
-  struct Grafo* novoGrafo = novo_grafo;
-  novoGrafo -> Nvert = V;
+//> addEdge ////////////////////////////////////////////////////////////////////
+void addEdge(struct Graph* graph, int origin, int destiny){
+  struct Vertex* newVertex = addVertex(destiny);
+  newVertex->next = graph->arrayAdjList[origin].head;
+  graph->arrayAdjList[destiny].head = newVertex;
+}
 
-  novoGrafo -> arrayAdjList = (struct listaAdja*) malloc(V * sizeof(struct listaAdja));
+//> newGraph ///////////////////////////////////////////////////////////////////
+struct Graph* newGraph(int N_Vertices) {
+  struct Graph* newGraph = newGraph_malloc;
+  newGraph -> Nvert = N_Vertices;
+  newGraph -> arrayAdjList = (struct listaAdja*) malloc(V * sizeof(struct listaAdja));
 
   int i;
   for (i=0; i < V; i++)
-    novoGrafo->arrayAdjList[i].head = NULL;
+    newGraph->arrayAdjList[i].head = NULL;
 
-    return novoGrafo;
+    return newGraph;
   }
-
-void adiciona_aresta(struct Grafo* grafo, int origem, int destino){
-  struct vertice* novoVer = criar_novo_vertice(destino);
-  novoVer->next = grafo->arrayAdjList[origem].head;
-  grafo->arrayAdjList[destino].head = novoVer;
-}
 
 void stackPush (int v_index){
 
@@ -64,30 +69,39 @@ int stackPop(){
 
 }
 
+void readData() {
+     scanf("%d\n", &N_Vertices);
+     scanf("%d\n", &N_Edges);
+ }
+
+ void readEdges(struct Graph* graph, int N_Edges){
+   for (int i = 1; i <= N_Edges; i++) {
+     scanf("%d %d\n", &vertex_1, &vertex_2);
+     addVertex(vertex_1);
+     addVertex(vertex_2);
+     addEdge(graph, vertex_1, vertex_2);
+   }
+ }
+
+ void testArguments(int N_Vertices, int N_Edges){
+   if(N_Vertices < 2 || N_Edges < 1){
+       fprintf(stderr, "Invalid Input\n");
+       exit(-1);
+   }
+}
 
 int main() {
-  int NVertices = 0;
-  int NArestas = 0;
+  int N_Vertices = 0;
+  int N_Edges = 0;
+  readData();
+  testArguments(N_Vertices, N_Edges);
+  struct Graph* graph = newGraph(N_Vertices);
+  readEdges(graph, N_Edges);
 
-  scanf("%d\n", &NVertices);
-  scanf("%d\n", &NArestas);
+  int vertexArray [N_Vertices];
 
-  struct vertice *listaAdja[NVertices], *p;
-
-  for( i = 1 ; i <= NVertices ; i++ )
-		listaAdja[i] = NULL;
-
-  for (int i = 1; i <= NArestas; i++) {
-    scanf("%d %d\n", &vertice1, &vertice2);
-    addAresta(vertice1, vertice2);
-  }
-
-  if(NVertices<2 && NArestas<1){
-    return -1;
-  }
-
-  printf("%d\n", NVertices );
-  printf("%d\n", NArestas);
+  printf("%d\n", N_Vertices );
+  printf("%d\n", N_Edges);
   //for (int i = 1; i <= ??; i++) {
   //  printf("%s\n", ??);
   //}
